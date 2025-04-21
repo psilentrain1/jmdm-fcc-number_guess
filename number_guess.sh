0d10f648af96d2cbcd21ffe75db3b1e405cbf3da
James Drake <github@jamesmdrakemedia.com>
2025-04-21 03:35:55 +0000

    feat: updated LOGIN func

--- number_guess.sh
+++ number_guess.sh
@@ -4,4 +4,19 @@ PSQL="psql --username=freecodecamp --dbname=number_guess -t --no-align -c"
 
 RANDOM_NUMBER=$((RANDOM % 1000 + 1))
 
-echo "$RANDOM_NUMBER"
+LOGIN(){
+  echo "Enter your username:"
+  read -n 22 USERNAME
+
+  USER=$($PSQL "SELECT user_id FROM users WHERE user_name = $USERNAME;")
+  if [[ -z $USER ]]
+  then
+    echo "User not exist"
+  else
+    echo "User exist"
+  fi
+}
+
+# GUESS(){}
+
+LOGIN
