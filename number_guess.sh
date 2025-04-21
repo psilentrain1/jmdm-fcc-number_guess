a79bf310cbf302255a1439327d84b25e037ff344
James Drake <github@jamesmdrakemedia.com>
2025-04-21 06:55:57 +0000

    fix: fixed logic errors

--- number_guess.sh
+++ number_guess.sh
@@ -16,66 +16,53 @@ LOGIN(){
     ADD_USER
   else
     DATA=$($PSQL "SELECT user_name, games_played, best_game FROM users WHERE user_id = $USER;")
-    echo "$DATA" | while read USER_NAME BAR GAMES_PLAYED BAR BEST_GAME
-    do
-      echo "Welcome back $USER_NAME! You have played $GAMES_PLAYED games, and your best game took $BEST_GAME guesses."
-      echo "Guess the secret number between 1 and 1000:"
-      read CURRENT_GUESS
-      GUESS_NO=$((GUESS_NO++))
-      CHECK $CURRENT_GUESS
-    done
+    read USER_NAME BAR GAMES_PLAYED BAR BEST_GAME <<< "$DATA"
+    echo "Welcome back $USER_NAME! You have played $GAMES_PLAYED games, and your best game took $BEST_GAME guesses."
+    echo "Guess the secret number between 1 and 1000:"
+    GUESS
   fi
 }
 
 ADD_USER(){
   USER_ADDED=$($PSQL "INSERT INTO users (user_name) VALUES ('$USERNAME');")
   echo "Guess the secret number between 1 and 1000:"
-  read -n 4 CURRENT_GUESS
-  GUESS_NO=$((GUESS_NO++))
-  CHECK $CURRENT_GUESS
+  GUESS
 }
 
 GUESS(){
   read -n 4 CURRENT_GUESS
-  GUESS_NO=$((GUESS_NO++))
+  GUESS_NO=$((GUESS_NO + 1))
   CHECK $CURRENT_GUESS
 }
 
 CHECK(){
   if (( $1 ))
   then
-    if [[ $((1)) -lt $RANDOM_NUMBER ]]
+    if [[ $1 -lt $RANDOM_NUMBER ]]
     then
       echo "$RANDOM_NUMBER"
       echo "It's higher than that, guess again:"
-      read -n 4 CURRENT_GUESS
-      GUESS_NO=$((GUESS_NO++))
-      CHECK $CURRENT_GUESS
-    elif [[ $((1)) -gt $RANDOM_NUMBER ]]
+      GUESS
+    elif [[ $1 -gt $RANDOM_NUMBER ]]
     then
       echo "It's lower than that, guess again:"
-      read -n 4 CURRENT_GUESS
-      GUESS_NO=$((GUESS_NO++))
-      CHECK $CURRENT_GUESS
-    elif [[ $((1)) -eq $RANDOM_NUMBER ]]
+      GUESS
+    elif [[ $1 -eq $RANDOM_NUMBER ]]
     then
       echo "You guessed it in $GUESS_NO tries. The secret number was $RANDOM_NUMBER. Nice job!"
       SAVE_PLAY
     fi
   else
     echo "That is not an integer, guess again:"
-    read -n 4 CURRENT_GUESS
-    GUESS_NO=$((GUESS_NO++))
-    CHECK $CURRENT_GUESS
-
+    GUESS
   fi
 }
 
 SAVE_PLAY(){
-  PLAYS=$($PSQL "SELECT games_played, best_game FROM users WHERE user_name = '$USER';")
+  PLAYS=$($PSQL "SELECT games_played, best_game FROM users WHERE user_name = '$USERNAME';")
   echo "$PLAYS" | while read GAMES_PLAYED BAR BEST_GAME
   do
-    GAMES_PLAYED=$((GAMES_PLAYED++))
+    GAMES_PLAYED=$((GAMES_PLAYED + 1))
     if [[ -z $BEST_GAME ]]
     then
       BEST_GAME=$GUESS_NO
@@ -85,7 +72,7 @@ SAVE_PLAY(){
         BEST_GAME=$GUESS_NO
       fi
     fi
-    PLAY_SAVED=$($PSQL "UPDATE users SET games_played = $((GAMES_PLAYED)), best_game = $((BEST_GAME)) WHERE user_name = '$USER';")
+    PLAY_SAVED=$($PSQL "UPDATE users SET games_played = $((GAMES_PLAYED)), best_game = $((BEST_GAME)) WHERE user_name = '$USERNAME';")
   done
 }
 
