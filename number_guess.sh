ce254fe03b20fae09d3e61322813d95b42f7a57e
James Drake <github@jamesmdrakemedia.com>
2025-04-21 04:21:47 +0000

    feat: finished login func

--- number_guess.sh
+++ number_guess.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-PSQL="psql --username=freecodecamp --dbname=number_guess -t --no-align -c"
+PSQL="psql --username=freecodecamp --dbname=number_guess --tuples-only -c"
 
 RANDOM_NUMBER=$((RANDOM % 1000 + 1))
 
@@ -8,14 +8,23 @@ LOGIN(){
   echo "Enter your username:"
   read -n 22 USERNAME
 
-  USER=$($PSQL "SELECT user_id FROM users WHERE user_name = $USERNAME;")
+  USER=$($PSQL "SELECT user_id FROM users WHERE user_name = '$USERNAME';")
   if [[ -z $USER ]]
   then
-    echo "User not exist"
+    echo "Welcome, $USERNAME! It looks like this is your first time here."
+    # ADD_USER
+    # GUESS
   else
-    echo "User exist"
+    DATA=$($PSQL "SELECT user_name, games_played, best_game FROM users WHERE user_id = $USER;")
+    echo "$DATA" | while read USER_NAME BAR GAMES_PLAYED BAR BEST_GAME
+    do
+      echo "Welcome back $USER_NAME! You have played $GAMES_PLAYED games, and your best game took $BEST_GAME guesses."
+      # GUESS 
+    done
   fi
 }
+
+# ADD_USER(){}
 
 # GUESS(){}
 
